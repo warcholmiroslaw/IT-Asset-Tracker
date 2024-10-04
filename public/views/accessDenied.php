353748bad62bfcc28ad29fbedd7537e94b54a6a0
Miroslaw Warchol <mirek.warcholl@gmail.com>
2024-10-04 15:47:51 +0200

    correct userView to responsive web design also for mobile

--- public/views/accessDenied.php
+++ public/views/accessDenied.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="public/css/background.css"> 
+    <link rel="stylesheet" type="text/css" href="public/css/menu.css"> 
+    <script src="public/scripts/logout.js" defer></script>
 
     <title>Access denied</title>
 
@@ -21,10 +23,20 @@
             font-size: 48px;
             color: red;
         }
+
+
     </style>
 
 </head>
 <body>
+
+    <div class = "menu">    
+            <h1>
+                Hi, <?php if (isset($_SESSION['user']) && !empty($_SESSION['user'])) {echo $_SESSION['user']['username'];} ?> !
+            </h1>
+            <button class = "logout">Logout</button>
+    </div>
+
     <h1>Access Denied !</h1> 
 </body>
 </html>
