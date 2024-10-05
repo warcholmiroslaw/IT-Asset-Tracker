3ab8945cd66b47034729e4f0905055ac0c0aa52a
Miroslaw Warchol <mirek.warcholl@gmail.com>
2024-10-05 13:41:36 +0200

    correct editDevice to responsive web design also for mobile

--- public/views/editDevice.php
+++ public/views/editDevice.php
@@ -3,7 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="public/css/editDevice.css"> 
+    <link rel="stylesheet" type="text/css" href="public/css/editDevice.css">
+    <link rel="stylesheet" type="text/css" href="public/css/background.css"> 
+ 
     <script defer src="public/scripts/validateDevice.js" defer></script>
     <title>Device properties</title>
 </head>
